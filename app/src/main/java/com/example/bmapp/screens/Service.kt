5f66dbe5305eda33041cc502a1c8917368c87d15
ino <littletasteofheaven@protonmail.com>
2022-05-27 20:29:11 +0600

    Draw phone verification dialog

--- app/src/main/java/com/example/bmapp/screens/Service.kt
+++ app/src/main/java/com/example/bmapp/screens/Service.kt
@@ -1,19 +1,19 @@
 package com.example.bmapp.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.* // ktlint-disable no-wildcard-imports
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.datastore.preferences.core.edit
+import androidx.compose.ui.window.Dialog
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.navigation.NavController
 import com.example.bmapp.dataStore
@@ -23,14 +23,17 @@ import kotlinx.coroutines.flow.map
 @Composable
 fun Service(navController: NavController, categoryName: String) {
     val dataStore = LocalContext.current.dataStore
+    val (verifiedSt, setVerifiedSt) = remember { mutableStateOf(false) }
+    val (verifyPhoneCardSt, setVerifyPhoneCardSt) = remember {
+        mutableStateOf(
+            false
+        )
+    }
+    val (otpSt, setOtpSt) = remember { mutableStateOf("") }
     val (phoneSt, setPhoneSt) = remember { mutableStateOf("") }
 
     LaunchedEffect(true) {
         val phonePref = stringPreferencesKey("phoneNumber")
-
-        dataStore.edit { settings ->
-            settings[phonePref] = "01934444444"
-        }
 
         val phone = dataStore.data.map { preferences ->
             preferences[phonePref] ?: ""
@@ -60,5 +63,56 @@ fun Service(navController: NavController, categoryName: String) {
                 text = "Phone Number"
             )
         })
+
+        when (verifiedSt) {
+            true -> {}
+            false -> {
+                OutlinedButton(onClick = { setVerifyPhoneCardSt(true) }) {
+                    Text(text = "Verify")
+                }
+            }
+        }
+    }
+
+    when (verifyPhoneCardSt) {
+        true -> {
+            Dialog(onDismissRequest = { setVerifyPhoneCardSt(false) }) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .background(color = Color(0xFFF5F5F5))
+                        .padding(15.dp)
+                ) {
+                    Text(text = "Enter the otp sent to your phone number $phoneSt")
+                    TextField(
+                        value = otpSt,
+                        onValueChange = { setOtpSt(it) },
+                        label = {
+                            Text(
+                                text = "OTP"
+                            )
+                        }
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(
+                            10.dp,
+                            alignment = Alignment.End
+                        )
+                    ) {
+
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(text = "Verify")
+                        }
+                        Button(onClick = { setVerifyPhoneCardSt(false) }) {
+                            Text(text = "Cancel")
+                        }
+                    }
+                }
+            }
+        }
+
+        false -> {
+        }
     }
 }
